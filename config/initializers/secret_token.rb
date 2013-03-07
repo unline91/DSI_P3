3a39049779a005c0ca2390e1bd0557b7b2e38fc9
Carlos <carlos@dsi.(none)>
2013-03-07 12:24:53 +0000

    p3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DsiGit3::Application.config.secret_token = '0bee2ea84e8f542d466ae98e20b5f7441bd3af79c0609ed509a0d52cf98ed1256691fb66ebd243c8e413cdff8ae9814ac1ec4bb680876dce8cf0b8c5f35e1d9f'
